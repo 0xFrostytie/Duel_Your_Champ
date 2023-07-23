66cd02ea131b32179f278b29af720d4d501d9065
0xFrostytie <100962848+0xFrostytie@users.noreply.github.com>
2023-07-23 08:57:26 +0200

    Added frontend

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Grid, Container, Box} from '@mui/material';
+import GameHistoryComponent from "./Components/GameHistoryComponent";
+import DojoBoyComponent from "./Components/DojoBoyComponent";
+import StakedComponent from "./Components/StakedComponent";
+
+function App() {
+  const [isInGame, setIsInGame] = React.useState(false);
+  return (
+    <div style={{backgroundColor:'#E1E1E1'}} className="w-full h-full">
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <DojoBoyComponent isDisabled={false} isInGame={isInGame} setIsInGame={setIsInGame} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box mb={4}>
+              <StakedComponent leftName="NFT #001" rightName="NFT #002" leftUrl="https://i.imgur.com/1Yz6JX1.png" rightUrl="https://i.imgur.com/1Yz6JX1.png" />
+            </Box>
+            <GameHistoryComponent />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <DojoBoyComponent isDisabled={true} isInGame={isInGame} setIsInGame={setIsInGame} />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/GameHistoryComponent.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import GameHistoryLine from "./GameHistoryLines";
+import { ReactComponent as PoliceIcon} from '../icons/police.svg';
+import {ReactComponent as AttackWhite} from '../icons/attack_white.svg';
+import {Box} from "@mui/material";
+
+const GameHistoryComponent = () => {
+  const [history, setHistory] = useState([{
+    createdAt: '12:04',
+    changeLeft: '+600',
+    iconLeft: PoliceIcon,
+    leftAddr: '0x12567',
+    MoveIcon: AttackWhite,
+    iconRight: PoliceIcon,
+    rightAddr: '0x12567',
+    changeRight: '-600',
+  }]);
+
+  const handleAdd = () => {
+    setHistory([{
+      createdAt: '12:05',
+      changeLeft: '+600',
+      iconLeft: PoliceIcon,
+      leftAddr: '0x12567',
+      MoveIcon: AttackWhite,
+      iconRight: PoliceIcon,
+      rightAddr: '0x12567',
+      changeRight: '-600',
+    }, ...history]);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', bgcolor: 'background.paper', textAlign:'center' }}>
+      <Typography variant="h6" component="div">
+        Game History
+      </Typography>
+      <List sx={{paddingX:'5px'}}>
+        {history.map((line, index) =>
+          <Box style={{backgroundColor: index % 2 === 0 ? 'gray' : 'white', borderRadius:'5%'}}>
+            <GameHistoryLine {...line} isGray={index % 2 === 0 ? 'gray' : 'white'} />
+          </Box>
+        )}
+      </List>
+      <Button variant="contained" onClick={handleAdd}>
+        Add Line
+      </Button>
+    </Paper>
+  );
+}
+
+export default GameHistoryComponent;
